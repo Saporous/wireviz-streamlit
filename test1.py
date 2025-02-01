1527653890f46d5ce03b223f3be6dc2fcc2680a8
Jeffrey C <Saporous@users.noreply.github.com>
2025-01-31 18:30:59 -0800

    add upload button and begin wireviz parse() test

--- test1.py
+++ test1.py
@@ -1,3 +1,15 @@
 import streamlit as st
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+import wireviz.wireviz as wv
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    st.write('uploaded!')
+    with open("temp.txt", "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    wv.parse("./temp.txt")
+
+with st.form("my_form"):
+    st.text_area('wireviz file content')
+    st.form_submit_button('Submit')
+    st.text_area('Preview')
+    st.form_submit_button('Download')
