5690a9803c0df4fdc2d03c47d7eb49a4a847e45c
Jeffrey C <Saporous@users.noreply.github.com>
2025-01-31 19:02:13 -0800

    parse doesn't error but idk where the image is going

--- test1.py
+++ test1.py
@@ -1,14 +1,21 @@
 import streamlit as st
 import wireviz.wireviz as wv
 
+with st.form("output options"):
+    outpng = st.checkbox("png")
+    st.form_submit_button('Save')
+
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     st.write('uploaded!')
     with open("temp.txt", "wb") as f:
         f.write(uploaded_file.getbuffer())
-    wv.parse("./temp.txt")
+    wv.parse("./temp.txt", "png", "png", "./", "test", "./")
 
-with st.form("my_form"):
+st.write('OR')
+
+with st.form("wireviz text input"):
+    st.write('Text input:')
     st.text_area('wireviz file content')
     st.form_submit_button('Submit')
     st.text_area('Preview')
